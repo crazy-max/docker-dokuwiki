6c8f12800f3f9b1e935939f199882e647fa63966
CrazyMax <crazy-max@users.noreply.github.com>
2021-02-19 22:09:19 +0100

    Fix DOKU_INC (#26)

--- rootfs/tpls/preload.php
+++ rootfs/tpls/preload.php
@@ -2,3 +2,4 @@
 
 if(!defined('DOKU_CONF')) define('DOKU_CONF', '/data/conf/');
 if(!defined('DOKU_LOCAL')) define('DOKU_LOCAL', '/data/conf/');
+if(!defined('DOKU_INC')) define('DOKU_INC', '/var/www/');
